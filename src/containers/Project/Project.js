0d2eb8aefb2e3a7f7724a06a190b0e7e1f8643bc
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2019-09-17 21:45:37 -0500

    Update Project.js

--- src/containers/Project/Project.js
+++ src/containers/Project/Project.js
@@ -43,7 +43,6 @@ const useStyles = makeStyles(theme => ({
         transform: 'rotate(180deg)',
     },
     avatar: {
-        // backgroundColor: red[500],
     },
     smallAvatar: {
         width: 25,
